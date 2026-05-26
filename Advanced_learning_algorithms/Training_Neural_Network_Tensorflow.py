19e4f0f6e13b63b736e18dec7c8ed595d221346f
DevrajCharan1208 <devrajcharan1208@gmail.com>
2026-05-26 18:56:40 +0530

    feat: add neural network training script using TensorFlow for binary classification

---
+++ Advanced_learning_algorithms/Training_Neural_Network_Tensorflow.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+np.random.seed(42)
+
+X = np.random.rand(200, 2) * 100  
+
+Y = ((X[:, 0] > 50) & (X[:, 1] > 30)).astype(int)
+Y = Y.reshape(-1, 1) 
+
+print("X shape:", X.shape) 
+print("Y shape:", Y.shape)
+
+model = Sequential([
+    Dense(units=25,activation='sigmoid'),
+    Dense(units=15,activation='sigmoid'),
+    Dense(units=1,activation='sigmoid')
+])
+
+from tensorflow.keras.losses import BinaryCrossentropy
+model.compile(loss = BinaryCrossentropy())
+model.fit(X,Y,epochs = 100)
+
+X_new = np.array([[60.0, 45.0]])
+prediction = model.predict(X_new)
+
+print("Probability :", prediction)
